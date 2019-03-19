1d28308827c0943799dcf50eb2c7776c25a01f10
文科 蒋 <839928622@qq.com>
2019-03-19 19:06:27 +0800

    add Config.GetUsers to Startup

--- BankOfDotNet.IdentityServer/Startup.cs
+++ BankOfDotNet.IdentityServer/Startup.cs
@@ -15,10 +15,13 @@ namespace BankOfDotNet.IdentityServer
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+
             services.AddIdentityServer()
                 .AddDeveloperSigningCredential()
                 .AddInMemoryApiResources(Config.GetAllApiResources())
-                .AddInMemoryClients(Config.GetClients());
+                .AddInMemoryClients(Config.GetClients())
+                .AddTestUsers(Config.GetUsers());
+                
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
